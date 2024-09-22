3cd06cdae524669d2b0829d0c9f388a903ae41c2
Munawir Fikri <munawir.fikri@aryanoble.co.id>
2024-09-22 21:47:13 +0700

    fix: backend

--- server.php
+++ server.php
@@ -1,8 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
@@ -12,15 +8,6 @@ $uri = urldecode(
 // application without having installed a "real" web server software here.
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header("Access-Control-Max-Age: 86400");  // Cache preflight response for 1 day
-    http_response_code(200);
-    exit();
 }
 
 
