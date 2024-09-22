dbbbc5a2d1491d89b17c4db6f395f8d5bfb4dec6
Munawir Fikri <munawir.fikri@aryanoble.co.id>
2024-09-22 21:25:34 +0700

    cors

--- server.php
+++ server.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
