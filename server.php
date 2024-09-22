8acc1114cb71337dae681646cfe83f5d1e96cf92
Munawir Fikri <munawir.fikri@aryanoble.co.id>
2024-09-22 21:27:12 +0700

    cors

--- server.php
+++ server.php
@@ -14,4 +14,14 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Max-Age: 86400");  // Cache preflight response for 1 day
+    http_response_code(200);
+    exit();
+}
+
+
 require_once __DIR__.'/public/index.php';
