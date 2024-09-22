310772bd27af8611acc73ec14c13df4f291be10c
Munawir Fikri <munawir.fikri@aryanoble.co.id>
2024-09-22 22:27:04 +0700

    fix: cors

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -11,39 +11,31 @@ final class CorsMiddleware implements MiddlewareInterface
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->ajax() && !$request->method(Request::OPTIONS)) {
-            return $next($request);
-        }
-
         $header = respond()->getHeader();
-        $header->set('Access-Control-Allow-Origin', '*');
-        $header->set('Access-Control-Allow-Origin', 'https://staging.munola.com');
+        $header->set('Access-Control-Allow-Origin', '*'); // Ganti dengan origin yang sesuai
+        $header->set('Access-Control-Allow-Origin', 'https://staging.munola.com'); // Ganti dengan origin yang sesuai
         $header->set('Access-Control-Expose-Headers', 'Authorization, Content-Type, Cache-Control, Content-Disposition');
 
+        // Set header Vary
         $vary = $header->has('Vary') ? explode(', ', $header->get('Vary')) : [];
         $vary = array_unique([...$vary, 'Accept', 'Origin', 'User-Agent', 'Access-Control-Request-Method', 'Access-Control-Request-Headers']);
         $header->set('Vary', join(', ', $vary));
 
-        if (!$request->method(Request::OPTIONS)) {
-            return $next($request);
-        }
+        // Handle preflight request
+        if ($request->method(Request::OPTIONS)) {
+            $header->set(
+                'Access-Control-Allow-Methods',
+                strtoupper($request->server->get('HTTP_ACCESS_CONTROL_REQUEST_METHOD', 'GET, POST, OPTIONS'))
+            );
 
-        // $header->unset('Content-Type');
+            $header->set(
+                'Access-Control-Allow-Headers',
+                $request->server->get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', 'Origin, Content-Type, Accept, Authorization, Accept-Language')
+            );
 
-        if (!$request->server->has('HTTP_ACCESS_CONTROL_REQUEST_METHOD')) {
             return respond()->setCode(Respond::HTTP_NO_CONTENT);
         }
 
-        $header->set(
-            'Access-Control-Allow-Methods',
-            strtoupper($request->server->get('HTTP_ACCESS_CONTROL_REQUEST_METHOD', $request->method()))
-        );
-
-        $header->set(
-            'Access-Control-Allow-Headers',
-            $request->server->get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', 'Origin, Content-Type, Accept, Authorization, Accept-Language')
-        );
-
-        return respond()->setCode(Respond::HTTP_NO_CONTENT);
+        return $next($request);
     }
 }
